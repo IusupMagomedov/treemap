ed38651cf9ef8a7122aa36027fca5572dc5b97ba
IusupMagomedov <yusub1988@gmail.com>
2022-12-17 16:08:43 +0300

    two tests passed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      React works!
+      <header>
+        <h4>Video Game Data Set | Movies Data Set | Kickstarter Data Set</h4>
+      </header>
+      <main>
+        <h1 id="title">Video Game Sales</h1>
+        <h3 id="description">Top 100 Most Sold Video Games Grouped by Platform</h3>
+      </main>
+      <footer>This project was developed by iUsup Magomedov as a FCC D3 certification project</footer>
     </div>
   );
 }
